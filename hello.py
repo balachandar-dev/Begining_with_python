65a78d8fc52e69cc08a3f9c5e68ec4b19554e786
Balachandar <balachandar.mutharasu@gmail.com>
2026-01-15 19:16:42 +0530

    Initial commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+# single line comment
+"""
+multiple line comment 
+"""
+
+
+name = "Alice"
+age = 20
+name = "Dave"
+
+total = 10 - 4
+
+my_long_string = "I am entering into AI"
+
+first_name = "Bob"
+last_name = "Smith"
+full_name = first_name + " " + last_name
+
+long_dash = "-" * len(full_name)
+
+is_true = True
+or_false = False
