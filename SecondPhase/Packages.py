64ae238fae1a19810c26d1401c2d07ddf228b9c1
Balachandar <balachandar.mutharasu@gmail.com>
2026-01-16 23:44:00 +0530

    Understood importing packages and requirement.txt

---
+++ SecondPhase/Packages.py
@@ -0,0 +1,12 @@
+# import math 
+
+# math.sqrt(4)
+
+from math import sqrt, pi
+
+sqrt(16)
+
+import random as rn
+
+random_number = rn.randint(1,10)
+random_string = rn.choice("Apple", "Banana", "Mango")
