907b3a3e95302bc6ced562f005b1a0009a10c57c
Balachandar <balachandar.mutharasu@gmail.com>
2026-01-16 11:44:47 +0530

    Datastructure - Lists

---
+++ Datastructures.py
@@ -0,0 +1,21 @@
+# List
+age = 25
+has_licence = True
+my_list = ["Alice", 25, age, has_licence]
+
+name = my_list[0]
+age = my_list[1]
+
+has_licence = my_list[-1]
+
+colors = ["red", "blue", "cyan", "teal"]
+
+primary_colors = colors[0:2]
+
+colors.append("green")
+
+colors.remove("green")
+
+colors.insert(2, "green")
+
+colors.sort()
